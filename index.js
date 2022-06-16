5c7fe82644bf8c64dc6d1cefedaad1ea175edb00
jonchurch <me@jonchurch.com>
2022-06-15 23:28:52 -0400

    remove the csv parsing elements

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-import { readFile, writeFile } from "fs/promises";
-import neatCsv from "neat-csv";
+import titlesData from "./titles.json";
 
-(async () => {
-  const csvContent = await neatCsv(await readFile("./cage.csv"));
-  console.log(csvContent[0]);
-
-  const movies = csvContent
-    .map(({ Movie }) => Movie)
-    .filter((value, index, self) => self.indexOf(value) === index);
-
-  await writeFile("./titles.json", JSON.stringify(movies, null, 2));
-
-  const sentences = new Array(10)
-    .fill()
-    .map(() => buildRandomSentence(movies, 3).join(" "));
-  console.log(sentences);
-})();
+// (async () => {
+//   const sentences = new Array(10)
+//     .fill()
+//     .map(() => buildRandomSentence(titlesData, 3).join(" "));
+//   console.log(sentences);
+// })();
 
 function getRandoIndex(length) {
   return Math.floor(Math.random() * length);
 }
 
-function buildRandomSentence(cages, numTitles = 3) {
-  const list = cages.slice();
+export function buildRandomSentence(data, numTitles = 3) {
+  const list = data.slice();
   const acc = [];
   for (let i = 0; i < numTitles; i += 1) {
     const cageIdx = getRandoIndex(list.length);
